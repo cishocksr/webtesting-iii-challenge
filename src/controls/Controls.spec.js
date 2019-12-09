7657ead1dd7fe2c318a7b295f2bb24cad5590a23
cishocksr <cishockleysr@gmail.com>
2019-12-08 21:28:37 -0500

    dashboard test passed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+
+import Controls from "./Controls";
+
+test("Controls.js renders", () => {
+  const wrapper = render(<Controls />);
+  expect(wrapper).toBeDefined();
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,24 @@
 // Test away
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, cleanup, getByText } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+
+afterEach(cleanup);
+
+test("Dashboard.js renders", () => {
+  const container = render(<Dashboard />);
+  expect(container).toBeDefined();
+  expect(container).toBeTruthy();
+});
+
+test("Shows the controls and display", () => {
+  const { container } = render(<Dashboard />);
+  const display = getByText(container, /open/i);
+  const controls = getByText(container, /close gate/i);
+
+  expect(display).toBeTruthy();
+  expect(display).toBeVisible();
+  expect(controls).toBeTruthy();
+  expect(controls).toBeVisible();
+});
