cd912e50171ad2a70c8bb54f5003debfacc8feb1
cishocksr <cishockleysr@gmail.com>
2019-12-08 21:04:01 -0500

    display and default test set

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
 // Test away!
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+
+import Display from "./Display";
+
+test("Display.js renders", () => {
+  const wrapper = render(<Display />);
+  expect(wrapper).toBeDefined();
+  expect(wrapper).not.toBeUndefined();
+  expect(wrapper).not.toBeFalsy();
+});
+
+test("Defaults to open and unlocked", () => {
+  const { getByText } = render(<Display />);
+  const open = getByText(/open/i);
+  const unlocked = getByText(/unlocked/i);
+
+  expect(open).toBeDefined();
+  expect(unlocked).toBeDefined();
+});
